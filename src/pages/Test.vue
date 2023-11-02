993035949598254794a5d56d8fbdd7cc5d6d0175
Sarman-Dasa <sarmand@zignuts.com>
2023-11-02 10:49:00 +0530

    first commit

---
+++ src/pages/Test.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+      <VCard class="mb-6" title="my custome page test">
+        <VCardText>All the best for your new project.</VCardText>
+      </VCard>
+    </div>
+  </template>
+s  
---
+++ src/pages/customePage/myPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <VCard class="mb-6" title="my custome page">
+      <VCardText>All the best for your new project.</VCardText>
+    </VCard>
+  </div>
+</template>
